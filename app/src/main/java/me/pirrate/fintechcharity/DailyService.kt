81173d81fb7a24a58bbdd2e9d753046a222551ec
Nick Shvelidze <captain@pirrate.me>
2018-09-16 17:01:31 +0400

    Service

--- app/src/main/java/me/pirrate/fintechcharity/DailyService.kt
+++ app/src/main/java/me/pirrate/fintechcharity/DailyService.kt
@@ -1,12 +1,16 @@
 package me.pirrate.fintechcharity
 
 import android.app.IntentService
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.support.v4.app.NotificationCompat
 import me.pirrate.fintechcharity.api.FintechAPI
 import me.pirrate.fintechcharity.api.models.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class DailyService : IntentService("DailyService") {
     override fun onHandleIntent(intent: Intent?) {
@@ -47,5 +51,14 @@ class DailyService : IntentService("DailyService") {
     fun transfer(amount: Float, beneficiary: Beneficiary) {
         val transferOut = TransferOut(1, beneficiary.IBAN, beneficiary.Name, Globals.userDetails!!.Name, amount, "GEL", "Charity" )
         FintechAPI.service.transferOut(transferOut).execute()
+
+        val notifManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val mBuilder = NotificationCompat.Builder(this)
+                .setContentTitle("თანხა გადარიცხულია")
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("სულ გადარიცხულია $amount"))
+                .setContentText("გადარიცხულია $amount")
+        notifManager.notify(1, mBuilder.build())
     }
 }
